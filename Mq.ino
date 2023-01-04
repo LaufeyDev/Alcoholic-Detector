7524b424a027c65a59366f44ec8a62f7cb1208c3
Laufey <94543623+LaufeyDev@users.noreply.github.com>
2023-01-04 18:11:48 +0530

    Add files via upload

---
+++ Mq.ino
@@ -0,0 +1,25 @@
+ //Using MQ Sensor
+int LED = ;  //led pin
+int ALCOHOL_sensor = ;// MQ-6 SENSOR  
+int ALCOHOL_detected;  
+void setup()  
+{  
+  Serial.begin(9600);  
+  pinMode(LED, OUTPUT);  
+  pinMode(ALCOHOL_sensor, INPUT);  
+}  
+void loop()  
+{  
+  ALCOHOL_detected = digitalRead(ALCOHOL_sensor);  
+  Serial.println(ALCOHOL_detected);  
+  if (ALCOHOL_detected == 1)  
+  {  
+    Serial.println("ALCOHOL detected...");  
+    digitalWrite(LED, HIGH);  
+  } 
+  else  
+  {  
+    Serial.println("No ALCOHOL detected ");  
+    digitalWrite(LED, LOW);  
+  }  
+}  
